49c9709e214473336011acfc724eb12586a3c6b5
d6er <d6er@qq.com>
2019-04-27 07:51:40 +0900

    update

--- config/client.js
+++ config/client.js
@@ -4,7 +4,8 @@ const config = {
   
   websocket_url: {
     'localhost': 'ws://localhost:3000/api',
-    'listman.d6er.com': 'wss://ws.listman.d6er.com/prod'
+    'listman.d6er.com': 'wss://ws.listman.d6er.com/prod',
+    'listdev.d6er.com': 'wss://ws.listdev.d6er.com'
   },
   
   lists: lists
