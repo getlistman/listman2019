939e5aa39e1c7b3b45d6607d7bc652d3608d82f6
d6er <d6er@qq.com>
2019-05-01 17:22:12 +0900

    replace domain

--- config/client.js
+++ config/client.js
@@ -4,8 +4,8 @@ const config = {
   
   websocket_url: {
     'localhost': 'ws://localhost:3000/api',
-    'listman.d6er.com': 'wss://ws.listman.d6er.com/prod',
-    'listdev.d6er.com': 'wss://ws.listdev.d6er.com'
+    'listman.y15e.io': 'wss://listman-ws.y15e.io',
+    'listdev.y15e.io': 'wss://listdev-ws.y15e.io'
   },
   
   lists: lists
