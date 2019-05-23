6573442cc4e97f8d10f7b2ffcd78316d01acdc91
Yoshihiro Watanabe <fd3s.boost@gmail.com>
2019-05-23 21:20:50 +0900

    update handler

--- src-server/handlerTS.ts
+++ src-server/handlerTS.ts
@@ -1,8 +1,14 @@
 import { Handler, Context } from 'aws-lambda'
 
-const ssr = require('./ssr')
-const cookie = require('cookie')
+const require_esm = require("esm")(module)
+
+const mongo = require('./mongo')
 const util = require('util')
+const ssr = require('./ssr')
+const api = require_esm('./api').default
+const websocket = require_esm('./websocket').default
+const cookie = require('cookie')
+const google = require('./auth/google')
 
 // https://github.com/DefinitelyTyped/DefinitelyTyped/blob/e01d6e7abb2343c6b845d4945a368ed55cbf5bd2/types/aws-lambda/index.d.ts#L482
 interface Response {
@@ -13,18 +19,68 @@ interface Response {
   body: string
 }
 
+let coldStart: boolean = true
+
 export const index: Handler = async (event: any = {}, context: Context): Promise<any> => {
   
-  const cookies: string = event.hasOwnProperty('headers') && event.headers.hasOwnProperty('Cookie')
-        ? cookie.parse(event.headers.Cookie) : ''
+  context.callbackWaitsForEmptyEventLoop = false
   
-  const ssrBody: string = await util.promisify(ssr)(event, cookies)
+  console.log('[handler.js] coldStart: ' + coldStart + ' ' + event.source)
+  coldStart = false
+
+  // CloudWatch event (ping)
+  if (event.source == 'aws.events') {
+    console.log('aws ping return');
+    return { statusCode: 200 }
+  }
+
+  // Logging
+  /*
+  console.log('[handler.js] event')
+  console.dir(event)
+  console.log('[handler.js] context')
+  console.dir(context)
+  */
   
-  const response: Response = {
-    statusCode: 200,
-    headers: { "Content-Type": "text/html" },
-    body: ssrBody
+  // WebSocket
+  if (event.hasOwnProperty('requestContext')) {
+    if (event.requestContext.eventType == 'CONNECT') {
+      await websocket(event)
+      return { statusCode: 200 }
+    } else if (event.requestContext.eventType == 'DISCONNECT') {
+      return { statusCode: 200 }
+    } else if (event.requestContext.eventType == 'MESSAGE') {
+      const wsResult = await websocket(event)
+      if (event.isOffline) {
+        return {
+          statusCode: 200,
+          headers: { "Content-Type": "text/html" },
+          body: JSON.stringify(wsResult)
+        }
+      } else {
+        return { statusCode: 200 }
+      }
+    }
   }
   
-  return response
+  // HTTP
+  if (event.path == '/auth/google') {
+    return google.index()
+  } else if (event.path == '/auth/google/callback') {
+    return google.callback(event)
+  } else {
+
+    const cookies: string = event.hasOwnProperty('headers') && event.headers.hasOwnProperty('Cookie')
+          ? cookie.parse(event.headers.Cookie) : ''
+    
+    const ssrBody: string = await util.promisify(ssr)(event, cookies)
+    
+    const response: Response = {
+      statusCode: 200,
+      headers: { "Content-Type": "text/html" },
+      body: ssrBody
+    }
+    
+    return response
+  }
 }
