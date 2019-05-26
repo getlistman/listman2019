c946e579942da6773bb3d057e613c4ae7d312e9b
d6er <d6er@qq.com>
2019-05-26 15:09:56 +0900

    Replace module.exports with export

--- src-server/api/account.js
+++ src-server/api/account.js
@@ -1,34 +1,28 @@
 const mongo = require('../mongo')
 const db = mongo.getConnection()
 
-const methods = {
-  
-  addAccount: (user_id, account) => {
-    return db.collection('accounts').updateOne(
-      {
-        user_id: user_id,
-        'profile.emailAddress': account.profile.emailAddress
-      },
-      {
-        $set: account
-      },
-      {
-        upsert: true
-      }
-    )
-  },
-  
-  fetchAccounts: ({ user_id }) => {
-    let query = {
-      user_id: user_id
+export function addAccount(user_id, account) {
+  return db.collection('accounts').updateOne(
+    {
+      user_id: user_id,
+      'profile.emailAddress': account.profile.emailAddress
+    },
+    {
+      $set: account
+    },
+    {
+      upsert: true
     }
-    return db.collection('accounts').find(query).toArray()
-  },
-  
-  removeAccount: (user_id, account) => {
-    
-  }
-  
+  )
 }
 
-module.exports = methods
+export function fetchAccounts({ user_id }) {
+  let query = {
+    user_id: user_id
+  }
+  return db.collection('accounts').find(query).toArray()
+}
+
+export function removeAccount(user_id, account) {
+  
+}
