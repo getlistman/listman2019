bf153a6fc368523b2c3e475a4b02feaf93337b1b
y15e <y15e@qq.com>
2019-06-09 18:51:15 +0900

    update js

--- dist/sw-register.js
+++ dist/sw-register.js
@@ -9,3 +9,13 @@ if ('serviceWorker' in navigator) {
     });
   });
 }
+
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  alert("beforeinstallprompt");
+});
--- dist/sw.js
+++ dist/sw.js
@@ -1,3 +1,17 @@
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = ['/'];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
 self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request)
